658984cf1cd5df55faba8ca1bc880378ae3c84c7
TheGuy - Shlomi <shlomi@theguy.co.il>
2021-12-06 15:32:50 +0200

    move text to Form component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Form from './Form';
 function App() {
 	return (
 		<section className='form'>
-			<h1>טופס פתיחת קריאת שירות</h1>
-			<h3>אנא מלאו את פרטי הקריאה באופן ברור ככל הניתן</h3>
 			<Form />
 		</section>
 	);
