677f93388b5673794158597a082db7d071fe550a
TheGuy - Shlomi <shlomi@theguy.co.il>
2021-12-07 11:41:07 +0200

    app: add google recaptcha v3

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './Form.css';
 import './scss/main.scss';
 import App from './App';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<GoogleReCaptchaProvider
+		reCaptchaKey={process.env.REACT_APP_RECAPTCHA_V3_PUB_KEY}
+	>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</GoogleReCaptchaProvider>,
 	document.getElementById('root'),
 );
